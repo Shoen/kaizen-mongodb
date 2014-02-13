85b9aec9ed0f121b85ff39d9d947bbc1f555bd62
Dawid Pogorzelski <shogun999@gmail.com>
2014-02-13 15:11:26 +0100

    small changes

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var MongoClient = require('mongodb').MongoClient;
 
 exports.connect = function(config) {
     var self = this;
+    self.collection = config.collection || 'log';
     MongoClient.connect(config.uri, function(err, db) {
         if (err) throw err;
         self.db = db;
     });
-    return 0;
+    return;
 };
 
 exports.save = function(obj) {
-    this.db.collection('log').insert(obj, {
-        w: 1
-    }, function(err) {
-        if (err) console.warn(err.message);
-        if (err && err.message.indexOf('E11000 ') !== -1) {}
+    var self = this;
+    this.db.collection(self.collection).insert(obj, {
+        w: 0
     });
+    return;
 };
